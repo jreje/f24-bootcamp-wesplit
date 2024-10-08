fae4a9e50148572b684923d638832fe08a06bf08
Julianne Rejesus <jreje@ad.unc.edu>
2024-10-08 15:18:14 -0400

    First Attempt

---
+++ WeSplit_4/WeSplit_4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit_4
+//
+//  Created by Julianne Rejesus  on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit_4/WeSplit_4/WeSplit_4App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_4App.swift
+//  WeSplit_4
+//
+//  Created by Julianne Rejesus  on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
